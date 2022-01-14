47dd175aadb6ac3b451706d47425b792f6db7d58
kevin <techatkevin@gmail.com>
2022-01-14 17:37:09 +0530

    removed test

--- src/test/java/com/hatake/zaikokanrisha/ZaikoKanrishaApplicationTests.java
+++ src/test/java/com/hatake/zaikokanrisha/ZaikoKanrishaApplicationTests.java
@@ -3,11 +3,18 @@ package com.hatake.zaikokanrisha;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class ZaikoKanrishaApplicationTests {
 
+//	@Test
+//	void contextLoads() {
+//	}
+
 	@Test
-	void contextLoads() {
+	void thisTest_ShouldPss() {
+		assertEquals(4, 2+2);
 	}
 
 }
